0c5d7e746e014233312f321945715b3b077f6210
Mohamed Gani Aarif S A <256300033+mohamedganiaarifsa-max@users.noreply.github.com>
2026-02-08 20:03:36 +0530

    Add Factorial.java file
    Signed-off-by: Mohamed Gani Aarif S A <256300033+mohamedganiaarifsa-max@users.noreply.github.com>

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 5; // The number to calculate
+        long result = 1;
+
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
